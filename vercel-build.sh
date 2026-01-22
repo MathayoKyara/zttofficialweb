7ea9fb8e3bca2b6573618644acf6406ab088d5d3
Mathayo Kyara <mathayokyara@gmail.com>
2026-01-22 13:56:23 +0300

    Fix vercel-build.sh execution and ignore rules

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz
 tar xf flutter_linux_3.24.5-stable.tar.xz
 export PATH="$PWD/flutter/bin:$PATH"
